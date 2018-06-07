52d1bc72471fd3b646578a2842ab61c4c185e108
mohammad <othman500othman500@gmail.com>
2018-06-07 04:28:35 -0700

    create database entity & edit computer page

--- templates/computers.php
+++ templates/computers.php
@@ -1,4 +1,51 @@
-<?php
 
-echo "computers page";
-?>
+<div style="margin:20px auto;background-color:#AAA;width:350px;padding:10px">
+<i> " computer offers"</i>
+</div>
+
+<h6>welcom for our  campany</h6>
+<br><br><br>
+<ul>
+<style>
+div
+{
+width=250px;
+padding:10px;
+background:#DDD;
+border:1px solid#CCC;
+margin:10px 0}
+</style>
+
+
+<label><b>computer_name:<b></label>
+<input type="text" placeholder="entre name">
+<br>
+<br>
+
+<label><b>computer_model:<b></label>
+<input type="text" placeholder="enter model">
+<br>
+<br>
+<label><b> computer_cpu:<b></label>
+<input type="text" placeholder="entre cpu">
+<br>
+<br>
+<label><b>computer_ram:<b></label>
+<input type="text"placeholder="entre ram">
+
+<br>
+<br>
+<label><b>computer_hard:<b></label>
+<input type="text"placeholder="entre hard">
+<br>
+<br>
+<label><b>computer_price:<b></label>
+<input type="text"placeholder="entre price">
+
+<br>
+<br>
+<input type="submit" name="OK"value="OK">
+<input type="reset">
+</ul>
+
+
