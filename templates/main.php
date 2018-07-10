1fea6eb146b2dd76941a4529006ea0876c22a9a0
Mohsen Ibrahim <imohsen9@gmail.com>
2018-07-10 13:13:28 +0300

    Add background image

--- templates/main.php
+++ templates/main.php
@@ -2,3 +2,6 @@
 style('ownnotes', 'bootstrap.min');
 print_unescaped($this->inc('navbar.inc'));
 ?>
+<div>
+<img class="img-responsive" src="<?php print_unescaped(image_path('ownnotes', 'ComputersMobiles.png')); ?>" />
+</div>
